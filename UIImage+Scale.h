72b423988442bdf616a42bf83a7c19f053fd57d9
Scott Gonyea <sgonyea@truecar.com>
2012-04-29 22:59:04 -0700

    Added code to scale images up/down.

---
+++ UIImage+Scale.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Scale.h
+//  Stockz
+//
+//  Created by Scott Gonyea on 4/29/12.
+//  Copyright (c) 2012 sgonyea inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (scale)
+
+-(UIImage*)scaleToSize:(CGSize)size;
+
+@end
